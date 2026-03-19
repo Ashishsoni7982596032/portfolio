13b49b118f9694c1e45baa1d68453930b983565a
Ashishsoni7982596032 <59621409+Ashishsoni7982596032@users.noreply.github.com>
2026-03-19 10:13:25 +0530

    Migrate gsap-trial imports to gsap
    Migrated SplitText and ScrollSmoother imports from gsap-trial to gsap.

--- src/components/utils/splitText.ts
+++ src/components/utils/splitText.ts
@@ -1,13 +1,8 @@
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-
-import { ScrollSmoother } from "gsap-trial/ScrollSmoother";
-// @ts-ignore
-import SplitText from "gsap-trial/SplitText";
-
 import { ScrollSmoother } from "gsap/ScrollSmoother";
 import { SplitText } from "gsap/SplitText";
-(fix: migrate from gsap-trial to gsap)
+
 
 interface ParaElement extends HTMLElement {
   anim?: gsap.core.Animation;
